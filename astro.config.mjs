e01ef97e997688a752a9f62226b0fccf3e9ebd01
RomanSloq <306553622+RomanSloq@users.noreply.github.com>
2026-09-21 23:01:09 +0500

    chore: migrate deployment to Deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel";
+import deno from "@deno/astro-adapter";
 
 export default defineConfig({
-  adapter: vercel(),
+  output: "server",
+  adapter: deno(),
 });
